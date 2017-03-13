9cdf04e9f9f1acf62e6a6ee747b7e7e3d01f0904
VahanAper <asryanvahandev@gmail.com>
2017-03-14 01:13:17 +0400

    video 04_08

--- src/components/TaskRow/Render.android.js
+++ src/components/TaskRow/Render.android.js
@@ -1,35 +1,55 @@
 import React from 'react';
 import {
-  View,
   Text,
   Image,
   TouchableHighlight,
+  Animated,
   StyleSheet
 } from 'react-native';
 
 import doneImage from '../../../images/done.png';
 
-const localStyle = StyleSheet.create({
-  doneButton: {
-    borderRadius: 5,
-    padding: 5
-  }
-});
-
 export default function render(baseStyle) {
+  const doneAnimation = new Animated.ValueXY();
+  const localStyle = StyleSheet.create({
+    doneButton: {
+      borderRadius: 5,
+      padding: 5
+    },
+    row: {
+      transform: doneAnimation.getTranslateTransform()
+    }
+  });
+
+  function animatedPress() {
+    Animated.spring(doneAnimation, {
+      tension: 2,
+      friction: 3,
+      toValue: {
+        x: -500,
+        y: 0
+      }
+    })
+    .start();
+
+    setTimeout(() => {
+      this.onDonePressed();
+    }, 1000);
+  }
+
   return (
-    <View style={baseStyle.container}>
+    <Animated.View style={[baseStyle.container, localStyle.row]}>
       <Text style={baseStyle.label}>
         {this.props.todo.task}
       </Text>
 
       <TouchableHighlight
         underlayColor="#DDDDDD"
-        onPress={this.onDonePressed.bind(this)}
+        onPress={animatedPress.bind(this)}
         style={localStyle.doneButton}
       >
         <Image source={doneImage} />
       </TouchableHighlight>
-    </View>
+    </Animated.View>
   );
 }
